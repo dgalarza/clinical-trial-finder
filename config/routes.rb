9afeac4eef7d3cfa4bc4fd1c8e46f0e36608c356
Michael Wenger <michaelwenger27@gmail.com>
2016-06-23 22:44:37 -0400

    Add unstyled navigation
    https://github.com/mwenger1/trial-match/issues/1

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,7 @@
 Rails.application.routes.draw do
+  root to: "homepage#show"
+
+  resources :articles, only: [:index]
+  resources :frequently_asked_questions, only: [:index]
+  resources :trials, only: [:index]
 end
---
+++ spec/features/user_browses_navigation_spec.rb
@@ -0,0 +1,25 @@
+require "rails_helper"
+
+RSpec.feature "User browses navigation" do
+  scenario "User browses navigation" do
+    visit root_path
+
+    expect(page).to have_content t("homepage.show.overview")
+
+    click_link t("application.navigation.trial_connector")
+
+    expect(page).to have_content t("titles.trials.index")
+
+    click_link t("application.navigation.blog")
+
+    expect(page).to have_content t("titles.articles.index")
+
+    click_link t("application.navigation.faq")
+
+    expect(page).to have_content t("titles.frequently_asked_questions.index")
+
+    click_link t("application.navigation.overview")
+
+    expect(page).to have_content t("titles.homepage.show")
+  end
+end
