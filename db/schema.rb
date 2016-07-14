644097ca3499bad9851f91f4919835a8e976ace5
Michael Wenger <michaelwenger27@gmail.com>
2016-07-13 22:13:38 -0400

    Use active record session store
    http://stackoverflow.com/a/18581914

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160714014028) do
+ActiveRecord::Schema.define(version: 20160714021131) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,16 @@ ActiveRecord::Schema.define(version: 20160714014028) do
     t.integer  "site_count"
     t.integer  "trial_count"
   end
+
+  create_table "sessions", force: :cascade do |t|
+    t.string   "session_id", null: false
+    t.text     "data"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "sessions", ["session_id"], name: "index_sessions_on_session_id", unique: true, using: :btree
+  add_index "sessions", ["updated_at"], name: "index_sessions_on_updated_at", using: :btree
 
   create_table "sites", force: :cascade do |t|
     t.integer  "trial_id",          null: false
