caccb7810f12f0f1878b3c21e8371c5f6e41fcae
Michael Wenger <michaelwenger27@gmail.com>
2016-08-14 20:57:54 -0400

    Remove clinical trials logo from index page

--- spec/features/user_views_trial_spec.rb
+++ spec/features/user_views_trial_spec.rb
@@ -12,12 +12,9 @@ RSpec.feature "User views trial" do
     trial_description = "Overview of trial"
     site_facility = "Site Facility"
     site_zip = "12345"
-    import = create(:import_log)
     trial = create(:trial, title: trial_title, description: trial_description)
     create(:site, facility: site_facility, zip_code: site_zip, trial: trial)
     visit trials_path
-
-    expect(page).to have_content l(import.created_at, format: :timestamp)
 
     click_link trial_title
 
