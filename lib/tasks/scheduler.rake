36e23a74540099803728614a13a44b0acc6bd257
Michael Wenger <michaelwenger27@gmail.com>
2016-10-18 22:33:49 -0400

    Ping cronitor after scheduler is run
    https://github.com/mwenger1/trial-match/issues/73

--- lib/tasks/scheduler.rake
+++ lib/tasks/scheduler.rake
@@ -2,5 +2,10 @@ namespace :scheduler do
   desc "Import all trials from clinicaltrials.gov"
   task :import_trials_from_clinicaltrials_gov => :environment do
     TrialsImporter.new.import
+    RestClient.get(cronitor_url)
+  end
+
+  def cronitor_url
+    "https://cronitor.link/#{ENV.fetch("CLINICALTRIALS_SYNC_CRONITOR_ID")}/complete"
   end
 end
--- spec/tasks/scheduler_rake_spec.rb
+++ spec/tasks/scheduler_rake_spec.rb
@@ -3,12 +3,31 @@ require "rails_helper"
 describe "scheduler:import_trials_from_clinicaltrials_gov" do
   include_context "rake"
 
+  before do
+    allow(RestClient).to receive(:get)
+  end
+
   it "imports from TrialsImporter" do
-    importer_spy = spy(:importer)
-    allow(TrialsImporter).to receive(:new).and_return(importer_spy)
+    with_environment "CLINICALTRIALS_SYNC_CRONITOR_ID" => "12345" do
+      importer_spy = spy(:importer)
+      allow(TrialsImporter).to receive(:new).and_return(importer_spy)
 
-    subject.invoke
+      subject.invoke
 
-    expect(importer_spy).to have_received(:import)
+      expect(importer_spy).to have_received(:import)
+    end
+  end
+
+  it "pings cronitor service" do
+    cronitor_id = "12345"
+    with_environment "CLINICALTRIALS_SYNC_CRONITOR_ID" => cronitor_id do
+      allow_any_instance_of(TrialsImporter).to receive(:import)
+
+      subject.invoke
+
+      expect(RestClient).to have_received(:get).with(
+        "https://cronitor.link/#{cronitor_id}/complete"
+      )
+    end
   end
 end
