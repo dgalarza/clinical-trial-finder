3dee161f52b4a15011387a4c287b93bb6ecab219
Michael Wenger <michaelwenger27@gmail.com>
2016-11-12 15:30:56 -0500

    Add segment events on expander.js
    https://github.com/mwenger1/clinical-trial-finder/issues/74

--- app/assets/javascripts/refills/expander.js
+++ app/assets/javascripts/refills/expander.js
@@ -1,5 +1,13 @@
 $(document).ready(function() {
   $('.js-expander-trigger').click(function(){
+    var clickType =
+      $(this).hasClass("expander-hidden") ? "Click Expand" : "Click Collapse";
+
+    analytics.track(
+      clickType,
+      { item: this.dataset.expandItem }
+    );
+
     $(this).toggleClass("expander-hidden");
   });
 });
